604233b3f16bac3f4b9b6a806115c377eb5d7cd4
tacheraSasi <tacherasasi@gmail.com>
2026-04-15 11:47:41 +0300

    feat: implement gRPC server for user service and add StartGRPCServer function

--- services/users/main.go
+++ services/users/main.go
@@ -1,17 +1,37 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	"net/http"
+	"log"
+	"works-on-my-machine/proto/user"
+	"works-on-my-machine/shared"
+
+	"google.golang.org/grpc"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello from users service!")
+type server struct {
+	user.UnimplementedUserServiceServer
+}
+
+func (s *server) GetUser(ctx context.Context, req *user.GetUserRequest) (*user.GetUserResponse, error) {
+	log.Println("Received request for user:", req.Id)
+
+	return &user.GetUserResponse{
+		Id:   req.Id,
+		Name: "Tach",
+	}, nil
 }
 
 func main() {
-	http.HandleFunc("/", handler)
+	listener, err := shared.StartGRPCServer("8081")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	grpcServer := grpc.NewServer()
+	user.RegisterUserServiceServer(grpcServer, &server{})
 
 	fmt.Println("Server running on :8081")
-	http.ListenAndServe(":8081", nil)
+	grpcServer.Serve(listener)
 }
---
+++ shared/server.go
@@ -0,0 +1,8 @@
+package shared
+
+import "net"
+
+
+func StartGRPCServer(port string) (net.Listener, error) {
+	return net.Listen("tcp",port)
+}
