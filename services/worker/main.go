1871894a85eb3fae0ea5533546dcb7d7439db98f
tacheraSasi <tacherasasi@gmail.com>
2026-04-17 12:04:45 +0300

    feat: implement Asynq server for processing email tasks and handle welcome email sending

--- services/worker/main.go
+++ services/worker/main.go
@@ -2,36 +2,46 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"log"
-	pb "works-on-my-machine/proto/user"
-	"works-on-my-machine/shared"
+	"time"
+	"works-on-my-machine/tasks"
 
-	"google.golang.org/grpc"
+	"github.com/hibiken/asynq"
 )
 
-type server struct {
-	pb.UnimplementedUserServiceServer
-}
-
-func (s *server) GetUser(ctx context.Context, req *pb.GetUserRequest) (*pb.GetUserResponse, error) {
-	log.Println("Received request for user:", req.Id)
-
-	return &pb.GetUserResponse{
-		Id:   req.Id,
-		Name: "Tach",
-	}, nil
-}
-
 func main() {
-	listener, err := shared.StartGRPCServer("8081")
-	if err != nil {
-		log.Fatal(err)
+	srv := asynq.NewServer(
+		asynq.RedisClientOpt{Addr: "redis:6379"},
+		asynq.Config{
+			Concurrency: 10, // processing 10 tasks at a time
+			Queues: map[string]int{
+				"critical": 6, // higher priority
+				"default":  3,
+				"low":      1,
+			},
+		},
+	)
+
+	mux := asynq.NewServeMux()
+	mux.HandleFunc("email:send", func(ctx context.Context, t *asynq.Task) error {
+		fmt.Printf("Received task: type=%s payload=%s\n", t.Type(), string(t.Payload()))
+		var p tasks.WelcomeEmailPayload
+		if err := json.Unmarshal(t.Payload(), &p); err != nil {
+			return fmt.Errorf("json.Unmarshal failed: %v", err)
+		}
+
+		// Simulating Doing the actual work
+		log.Printf("Sending welcome email to %s (user %s)", p.Email, p.UserID)
+		time.Sleep(5 * time.Second)
+		log.Printf("Welcome email sent to %s (user %s)", p.Email, p.UserID)
+
+		// Return nil = success, return error = retry
+		return nil
+	})
+
+	if err := srv.Run(mux); err != nil {
+		log.Fatalf("could not run server: %v", err)
 	}
-
-	grpcServer := grpc.NewServer()
-	pb.RegisterUserServiceServer(grpcServer, &server{})
-
-	fmt.Println("Server running on :8081")
-	grpcServer.Serve(listener)
 }
